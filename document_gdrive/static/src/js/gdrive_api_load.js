a5fc2f3931f41e3f08373ce77cb2f5f556850606
be-cloud-js <js@192.168.0.5>
2015-04-26 16:07:19 +0200

    t

---
+++ document_gdrive/static/src/js/gdrive_api_load.js
@@ -0,0 +1,38 @@
+// The Browser API key obtained from the Google Developers Console.
+var developerKey = 'xxxxxxxYYYYYYYY-12345678';
+
+// The Client ID obtained from the Google Developers Console. Replace with your own Client ID.
+var clientId = "1234567890-abcdefghijklmnopqrstuvwxyz.apps.googleusercontent.com"
+
+// Scope to use to access user's photos.
+var scope = ['https://www.googleapis.com/auth/photos'];
+
+var pickerApiLoaded = false;
+var oauthToken;
+
+// Use the API Loader script to load google.picker and gapi.auth.
+function onApiLoad() {
+  gapi.load('auth', {'callback': onAuthApiLoad});
+  gapi.load('picker', {'callback': onPickerApiLoad});
+}
+
+function onAuthApiLoad() {
+  window.gapi.auth.authorize(
+      {
+        'client_id': clientId,
+        'scope': scope,
+        'immediate': false
+      },
+      handleAuthResult);
+}
+
+function onPickerApiLoad() {
+  pickerApiLoaded = true;
+  createPicker();
+}
+
+function handleAuthResult(authResult) {
+  if (authResult && !authResult.error) {
+    oauthToken = authResult.access_token;
+  }
+}
