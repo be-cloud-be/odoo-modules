20cd9855d1bcd54909ce01ddc5242748ff96909c
be-cloud-js <js@192.168.0.5>
2015-04-26 16:02:59 +0200

    t

--- document_gdrive/static/src/js/gdrive.js
+++ document_gdrive/static/src/js/gdrive.js
@@ -2,49 +2,8 @@ openerp.document_gdrive = function(instance, m) {
     var _t = instance.web._t,
         QWeb = instance.web.qweb;
 
- // The Browser API key obtained from the Google Developers Console.
-    var developerKey = 'xxxxxxxYYYYYYYY-12345678';
-
-    // The Client ID obtained from the Google Developers Console. Replace with your own Client ID.
-    var clientId = "1234567890-abcdefghijklmnopqrstuvwxyz.apps.googleusercontent.com"
-
-    // Scope to use to access user's photos.
-    var scope = ['https://www.googleapis.com/auth/photos'];
-
-    var pickerApiLoaded = false;
-    var oauthToken;
-
-    // Use the API Loader script to load google.picker and gapi.auth.
-    function onApiLoad() {
-      gapi.load('auth', {'callback': onAuthApiLoad});
-      gapi.load('picker', {'callback': onPickerApiLoad});
-    }
-
-    function onAuthApiLoad() {
-      window.gapi.auth.authorize(
-          {
-            'client_id': clientId,
-            'scope': scope,
-            'immediate': false
-          },
-          handleAuthResult);
-    }
-
-    function onPickerApiLoad() {
-      pickerApiLoaded = true;
-      createPicker();
-    }
-
-    function handleAuthResult(authResult) {
-      if (authResult && !authResult.error) {
-        oauthToken = authResult.access_token;
-        createPicker();
-      }
-    }
-
     // Create and render a Picker object for picking user Photos.
     function createPicker() {
-      onApiLoad();
       if (pickerApiLoaded && oauthToken) {
         var picker = new google.picker.PickerBuilder().
             addView(google.picker.ViewId.PHOTOS).
