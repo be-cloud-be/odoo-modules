0f7bacf0d6c70ec38f92bc8ad8004e7580ad4c86
be-cloud-js <js@192.168.0.5>
2015-04-27 07:37:22 +0200

    t

--- document_gdrive/static/src/js/gdrive.js
+++ document_gdrive/static/src/js/gdrive.js
@@ -18,16 +18,11 @@ openerp.document_gdrive = function(instance, m) {
               url = doc[google.picker.Document.URL];
             }
             var self = this;
-            var view = self.getParent();
-            var ids = ( view.fields_view.type != "form" )? view.groups.get_selection().ids : [ view.datarecord.id ];
-            var model = new instance.web.Model("ir.attachment.add_gdrive");
-            model.do_action(result, {
-                    additional_context: {
-                        'active_ids': ids,
-                        'active_id': [ids[0]],
-                        'active_model': view.dataset.model,
-                    },
-            });
+            var model = new openerp.web.Model("ir.attachment.add_gdrive");
+            model.call('action_add_gdrive', [url],
+                    {context: openerp.session.user_context}).then(function (result) {
+		      // do something with change_password result
+		    });
         },
         on_gdrive_doc: function() {
         	if(!pickerApiLoaded || !oauthToken) {
