1ff0af36850ff0829a7d3611af67b593fb690ec2
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2015-05-01 12:37:16 +0200

    Clean up api loading and token aquisition code

--- document_gdrive/static/src/js/gdrive_api_load.js
+++ document_gdrive/static/src/js/gdrive_api_load.js
@@ -1,4 +1,4 @@
-// The Client ID obtained from the Google Developers Console. Replace with your own Client ID.
+// The Client ID of Imply.lu
 var clientId = "433547367780-7tc38g697r7aoov2oqglb6s1bafktich.apps.googleusercontent.com"
 
 var scope = ['https://www.googleapis.com/auth/drive'];
@@ -10,7 +10,6 @@ var oauthToken;
 function onApiLoad() {
   gapi.load('auth', {'callback': onAuthApiLoad});
   gapi.load('picker', {'callback': onPickerApiLoad});
-  gapi.auth.init(function() {}) 
 }
 
 function onAuthApiLoad() {
@@ -30,5 +29,7 @@ function onPickerApiLoad() {
 function handleAuthResult(authResult) {
   if (authResult && !authResult.error) {
     oauthToken = authResult.access_token;
+  } else {
+	alert('Cannot get authorization token for Google Drive:'+authResult && !authResult.error);
   }
 }
