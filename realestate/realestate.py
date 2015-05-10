4f335f23525d3119b9d41168987c7c2244e69bb0
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2015-05-10 08:51:55 +0200

    t

--- realestate/realestate.py
+++ realestate/realestate.py
@@ -38,27 +38,18 @@ class realestate_abstract_asset(models.AbstractModel):
     name = fields.Char(string="Name", description="Short description of the asset.")
     description = fields.Text(string="Description", description="Description of the asset.")
     
-    owner_id = fields.Many2one('res.partner', string ="Owner", description="The land owner")
-    address_id = fields.Many2one('res.partner', string = "Address",description="The land address",required=True, ondelete='cascade')
+    type = fields.Selection(string="Asset Type", description="The type of asset",selection=[ ('composite','Composite'), ('land','Land'), ('house','House'), ('flat','Flat'), ('parking','Parking'),], select=True)
     
-    public_price = fields.Integer(string = "Price", description="The public price.")
-    estimated_price = fields.Integer(string = "Estimation", description="The estimated price.")
-
-    street = fields.Char(string = 'Street')
-    street2 = fields.Char(string = 'Street2')
-    zip = fields.Char(string = 'Zip', size=24, change_default=True)
-    city = fields.Char(string = 'City')
-    state_id = fields.Many2one("res.country.state", string = 'State', ondelete='restrict')
-    country_id = fields.Many2one('res.country', string = 'Country', ondelete='restrict')
+    owner_id = fields.Many2one('res.partner', string ="Owner", description="The land owner")
     
     active = fields.Boolean(string = "Is Active", description="Set to false if the assets is not available (destroyed, splitted, lost,...)")
     
-    @api.multi
-    def onchange_state(self, state_id):
-        if state_id:
-            state = self.env['res.country.state'].browse(state_id)
-            return {'value': {'country_id': state.country_id.id}}
-        return {}
+    @api.model
+    def default_get(self, fields):
+        res = super(realestate_asset, self).default_get(fields)
+        if 'type' in fields and 'default_type' in self.env.context:
+            res['type'] = self.env.context['default_type']
+        return res
 
 realestate_abstract_asset()
 
@@ -68,8 +59,11 @@ class realestate_asset(models.Model):
     
     _inherit = {'realestate.realestate_abstract_asset'} 
     
-    to_buy = fields.Boolean(string="To Buy", description="This asset can be bought")
-    to_sale = fields.Boolean(string="To Sale", description="This asset can be sold")
+    to_buy = fields.Boolean(string="To Buy", description="This asset can be bought.")
+    to_sale = fields.Boolean(string="To Sale", description="This asset can be sold.")
+        
+    buy_price = fields.Integer(string = "Buy Price", description="The public buy price.")
+    sale_price = fields.Integer(string = "Sale Price", description="The public sale price.")
         
     parent_id = fields.Many2one('realestate.realestate_asset', string = 'Procurement Group')
     component_ids = fields.One2many('realestate.realestate_asset', 'parent_id', string = "Components", description="The component of the asset.")
@@ -80,7 +74,14 @@ class building_land(models.Model):
     '''Building Land'''
     _name = 'realestate.building_land'
         
-    _inherit = {'realestate.realestate_asset'}
+    _inherit = {'realestate.realestate_abstract_asset'}
+    
+    street = fields.Char(string = 'Street')
+    street2 = fields.Char(string = 'Street2')
+    zip = fields.Char(string = 'Zip', size=24, change_default=True)
+    city = fields.Char(string = 'City')
+    state_id = fields.Many2one("res.country.state", string = 'State', ondelete='restrict')
+    country_id = fields.Many2one('res.country', string = 'Country', ondelete='restrict')
     
     land_division = fields.Char(string = "Land Division", description="The land division reference.")
     land_size = fields.Integer(string = "Size", description="Size in ares.")
@@ -91,20 +92,25 @@ class building_land(models.Model):
     parent_land_id = fields.Many2one('realestate.building_land', string = "Parent", description="The parent land in case it has been splitted.")
     subdivision_ids = fields.One2many('realestate.building_land', 'parent_land_id', string = "Subdivisions", description='The subdivision of the land.')
        
+    @api.multi
+    def onchange_state(self, state_id):
+        if state_id:
+            state = self.env['res.country.state'].browse(state_id)
+            return {'value': {'country_id': state.country_id.id}}
+        return {}
+           
 building_land()
 
 class building(models.AbstractModel):
     '''Building'''
     _name ='realestate.building'
     
-    _inherit = {'realestate.realestate_asset'}
+    _inherit = {'realestate.realestate_abstract_asset'}
     
     land_id = fields.Many2one('realestate.building_land', string = "Land", description="The land the building is built upon.")
 
-    type = fields.Selection(string="Building Type", description="The type of building",selection=[ ('house','House'), ('flat','Flat'), ('parking','Parking'),], select=True)
-
     rooms = fields.Integer(string="Rooms", description="Number of rooms")
     size = fields.Integer(string="Size", description="Size in square meters")
-    parking = fields.Integer(string="Parkings", description="Number of parkings")
+    parkings = fields.Integer(string="Parkings", description="Number of parkings")
 
 building()
