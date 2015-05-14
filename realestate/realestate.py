46a352f772782a3af2b9a83457f3147560ff0b6b
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2015-05-14 11:56:25 +0200

    t

--- realestate/realestate.py
+++ realestate/realestate.py
@@ -76,9 +76,9 @@ class realestate_asset(models.Model):
     def unlink(self):
         abstract_ids = []
         for asset in self:
-            abstract_ids.append(asset.asset_id)
+            abstract_ids.append(asset.asset_id.id)
         super(realestate_asset, self).unlink()
-        return self.env['realestate.realestate_abstract_asset'].browse(abstract_ids).unlink()
+        return self.pool.get('realestate.realestate_abstract_asset').unlink(self.env.cr, self.env.uid, abstract_ids, context=self.env._context)
     
     to_buy = fields.Boolean(string="To Buy", description="This asset can be bought.", select=True)
     to_sale = fields.Boolean(string="To Sale", description="This asset can be sold.", select=True)
@@ -104,11 +104,11 @@ class building_land(models.Model):
     def unlink(self):
         abstract_ids = []
         for asset in self:
-            abstract_ids.append(asset.asset_id)
+            abstract_ids.append(asset.asset_id.id)
         super(building_land, self).unlink()
-        import wdb
-        wdb.set_trace()
-        return self.env['realestate.realestate_abstract_asset'].browse(abstract_ids).unlink()
+        return self.pool.get('realestate.realestate_abstract_asset').unlink(self.env.cr, self.env.uid, abstract_ids, context=self.env._context)
+    
+        super(realestate_abstract_asset, self).unlink(self.env.cr, self.env.uid, abstract_ids, context=self.env.context)
     
     street = fields.Char(string = 'Street')
     street2 = fields.Char(string = 'Street2')
@@ -150,9 +150,9 @@ class parking(models.Model):
     def unlink(self):
         abstract_ids = []
         for asset in self:
-            abstract_ids.append(asset.asset_id)
+            abstract_ids.append(asset.asset_id.id)
         super(parking, self).unlink()
-        return self.env['realestate.realestate_abstract_asset'].browse(abstract_ids).unlink()
+        return self.pool.get('realestate.realestate_abstract_asset').unlink(self.env.cr, self.env.uid, abstract_ids, context=self.env._context)
     
     land_id = fields.Many2one('realestate.building_land', string = "Land", description="The land the parking is upon.")
     city = fields.Char(related='land_id.city', store=True, select=True)
@@ -175,9 +175,9 @@ class building(models.Model):
     def unlink(self):
         abstract_ids = []
         for asset in self:
-            abstract_ids.append(asset.asset_id)
+            abstract_ids.append(asset.asset_id.id)
         super(building, self).unlink()
-        return self.env['realestate.realestate_abstract_asset'].browse(abstract_ids).unlink()
+        return self.pool.get('realestate.realestate_abstract_asset').unlink(self.env.cr, self.env.uid, abstract_ids, context=self.env._context)
     
     land_id = fields.Many2one('realestate.building_land', string = "Land", description="The land the building is built upon.")
     city = fields.Char(related='land_id.city', store=True, select=True)
