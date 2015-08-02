a30b99651d627452405412746aa17e20ca808e73
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2015-08-02 22:39:22 +0000

    fix value_date computation

--- account_bank_transfert/account_bank_transfert.py
+++ account_bank_transfert/account_bank_transfert.py
@@ -23,15 +23,6 @@ class account_bank_transfert(models.Model):
         },
     }
     
-    
-    @api.model
-    def _default_journal(self):
-        company_id = self._context.get('company_id', self.env.user.company_id.id)
-        domain = [
-            ('company_id', '=', company_id),
-        ]
-        return self.env['account.journal'].search(domain, limit=1)
-        
     @api.model
     def _default_currency(self):
         journal = self._default_journal()
@@ -65,11 +56,6 @@ class account_bank_transfert(models.Model):
         required=True, readonly=True, states={'draft': [('readonly', False)]},
         default=_default_currency, track_visibility='always')
         
-    journal_id = fields.Many2one('account.journal', string='Journal',
-        required=True, readonly=True, states={'draft': [('readonly', False)]},
-        default=_default_journal,
-        domain="[('company_id', '=', company_id)]")
-    
     company_id = fields.Many2one('res.company', string='Company', change_default=True,
         required=True, readonly=True, states={'draft': [('readonly', False)]},
         default=lambda self: self.env['res.company']._company_default_get('account.bank_transfert'))
@@ -97,6 +83,8 @@ class account_bank_transfert(models.Model):
     amount = fields.Float(string='Amount which will be transfered.', digits=dp.get_precision('Account'),
         readonly=True, states={'draft': [('readonly', False)]}, default=0.0)
 
+    journal_entry_id = fields.Many2one('account.move', string='Journal Entry', copy=False)
+
     note = fields.Text(string='Notes')
     
     _sql_constraints = [
@@ -120,4 +108,134 @@ class account_bank_transfert(models.Model):
                 tr.write(res['value'])
         return True
         
-    
+    @api.model
+    def _prepare_move(self):
+        """Prepare the dict of values to create the move from a
+           statement line. This method may be overridden to implement custom
+           move generation (making sure to call super() to establish
+           a clean extension chain).
+           :param browse_record st_line: account.bank.statement.line record to
+                  create the move from.
+           :param char st_line_number: will be used as the name of the generated account move
+           :return: dict of value to create() the account.move
+        """
+        return {
+            'journal_id': self.journal_id.id,
+            'period_id': self.period_id.id,
+            'company_id': self.company_id.id,
+            'date': self.trade_date,
+            'name': None,
+            'ref': None,
+        }
+        
+    @api.multi
+    def action_move_create(self):
+        """ Creates transfert related analytics and financial move lines """
+        am_obj = self.env['account.move']
+        aml_obj = self.env['account.move.line']
+        
+        for tr in self:
+            if not tr.from_account_id.journal_id.default_debit_account_id:
+                raise except_orm(_('Error!'), _('Please define an internal transfert account on the journal related to the origin account.'))
+            if not tr.to_account_id.journal_id.default_debit_account_id:
+                raise except_orm(_('Error!'), _('Please define an internal transfert account on the journal related to the destination account.'))
+            if not tr.from_account_id.journal_id.sequence_id:
+                raise except_orm(_('Error!'), _('Please define sequence on the journal related to the origin account.'))
+            if not tr.to_account_id.journal_id.sequence_id:
+                raise except_orm(_('Error!'), _('Please define sequence on the journal related to the destination account.'))
+            if tr.move_id:
+                continue
+            
+            move_vals = bs_obj._prepare_move(tr)
+            move_id = am_obj.create(move_vals)
+            
+            from_line = {
+                'journal_id': tr.from_account_id.journal_id,
+                'period_id': tr.period_id.id,
+                'name': _('change') + ': ' + (tr.name or '/'),
+                'account_id': tr.from_account_id.journal_id.default_debit_account_id.id,
+                'move_id': move_id,
+                'partner_id': tr.company_id,
+                'currency_id': tr.currency_id,
+                'amount_currency': tr.amount,
+                'quantity': tr.amount,
+                'credit': 0,
+                'debit': tr.amount,
+                'date': tr.trade_date,
+                'counterpart_move_line_id': mv_line.id,
+            }
+            aml_obj.create(from_line)
+            
+            from_line_counterpart = {
+                'journal_id': tr.from_account_id.journal_id,
+                'period_id': tr.period_id.id,
+                'name': _('change') + ': ' + (st_line.name or '/'),
+                'account_id': tr.from_account_id.journal_id.internal_account_id.id,
+                'move_id': move_id,
+                'amount_currency': tr.amount,
+                'partner_id': tr.company_id,
+                'currency_id': tr.currency_id,
+                'quantity': tr.amount,
+                'debit': tr.amount,
+                'credit': 0,
+                'date': tr.trade_date,
+            }
+            aml_obj.create(from_line_counterpart)
+            
+            to_line = {
+                'journal_id': tr.to_account_id.journal_id,
+                'period_id': tr.period_id.id,
+                'name': _('change') + ': ' + (tr.name or '/'),
+                'account_id': tr.from_account_id.journal_id.internal_account_id.id,
+                'move_id': move_id,
+                'partner_id': tr.company_id,
+                'currency_id': tr.currency_id,
+                'amount_currency': tr.amount,
+                'quantity': tr.amount,
+                'credit': 0,
+                'debit': tr.amount,
+                'date': tr.trade_date,
+                'counterpart_move_line_id': mv_line.id,
+            }
+            aml_obj.create(to_line)
+            
+            to_line_counterpart = {
+                'journal_id': tr.to_account_id.journal_id,
+                'period_id': tr.period_id.id,
+                'name': _('change') + ': ' + (st_line.name or '/'),
+                'account_id': tr.from_account_id.journal_id.default_debit_account_id.id,
+                'move_id': move_id,
+                'amount_currency': tr.amount,
+                'partner_id': tr.company_id,
+                'currency_id': tr.currency_id,
+                'quantity': tr.amount,
+                'debit': tr.amount,
+                'credit': 0,
+                'date': tr.trade_date,
+            }
+            aml_obj.create(to_line_counterpart)
+        
+            self.write({'journal_entry_id': move_id})
+        
+    @api.multi
+    def action_cancel(self):
+        moves = self.env['account.move']
+        for inv in self:
+            if inv.move_id:
+                moves += inv.move_id
+            if inv.payment_ids:
+                for move_line in inv.payment_ids:
+                    if move_line.reconcile_partial_id.line_partial_ids:
+                        raise except_orm(_('Error!'), _('You cannot cancel a transfert which is partially paid. You need to unreconcile related payment entries first.'))
+
+        # First, set the invoices as cancelled and detach the move ids
+        self.write({'state': 'cancel', 'move_id': False})
+        if moves:
+            # second, invalidate the move(s)
+            moves.button_cancel()
+            # delete the move this invoice was pointing to
+            # Note that the corresponding move_lines and move_reconciles
+            # will be automatically deleted too
+            moves.unlink()
+        self._log_event(-1.0, 'Cancel Transfert')
+        return True
