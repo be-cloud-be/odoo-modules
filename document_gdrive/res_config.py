1cb30468f8a6d142445d5de691c66c2aaf369164
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2015-08-13 16:38:17 +0000

    add config parameter

--- document_gdrive/res_config.py
+++ document_gdrive/res_config.py
@@ -3,6 +3,6 @@ from openerp.osv import fields, osv
 class knowledge_config_settings(osv.osv_memory):
     _inherit = 'knowledge.config.settings'
     _columns = {
-        'document_gdrive_upload_dir': fields.string('Google Drive Upload Directory',
+        'document_gdrive_upload_dir': fields.char('Google Drive Upload Directory',
             help='Directory where the files will be uploaded using the Google File Picker.'),
     }
