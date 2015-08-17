fcb60886c32468ceb69f5a564a0fc46a5f584003
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2015-08-17 13:38:17 +0000

    add client id

--- document_gdrive/static/src/js/gdrive.js
+++ document_gdrive/static/src/js/gdrive.js
@@ -49,7 +49,7 @@ openerp.document_gdrive = function(instance, m) {
             }
         },
         on_gdrive_doc: function() {
-        	if(!pickerApiLoaded) // || !oauthToken) {
+        	if(!pickerApiLoaded) { // || !oauthToken) {
           	  onApiLoad();
             }
         	var self = this;
