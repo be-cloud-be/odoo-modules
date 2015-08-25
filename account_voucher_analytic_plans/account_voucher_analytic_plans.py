d1811c500fe63db53d86dcac9fca6504e1626d26
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2015-08-25 18:04:01 +0000

    add inherited view on account.voucher.form

--- account_voucher_analytic_plans/account_voucher_analytic_plans.py
+++ account_voucher_analytic_plans/account_voucher_analytic_plans.py
@@ -27,24 +27,21 @@ class account_voucher(osv.osv):
         'analytics_id': fields.many2one('account.analytic.plan.instance', 'Analytic Distribution'),
     }
 
-    def first_move_line_get(self, cr, uid, voucher_id, move_id, company_currency, current_currency, context=None):
-        move_line = super(account_voucher, self).first_move_line_get(cr, uid, voucher_id, move_id, company_currency, current_currency, context=context)
+    # def first_move_line_get(self, cr, uid, voucher_id, move_id, company_currency, current_currency, context=None):
+    #     move_line = super(account_voucher, self).first_move_line_get(cr, uid, voucher_id, move_id, company_currency, current_currency, context=context)
 
-        voucher = self.pool.get('account.voucher').browse(cr, uid, voucher_id, context=context)
-        v_line = voucher.line_ids[0]
-        if v_line.account_id.type != 'payable':
-            move_line['analytics_id'] = v_line.analytics_id and v_line.analytics_id.id or False
-        return move_line
+    #     voucher = self.pool.get('account.voucher').browse(cr, uid, voucher_id, context=context)
+    #     v_line = voucher.line_ids[0]
+    #     if v_line.account_id.type != 'payable':
+    #         move_line['analytics_id'] = v_line.analytics_id and v_line.analytics_id.id or False
+    #     return move_line
 
     def voucher_move_line_create(self, cr, uid, voucher_id, line_total, move_id, company_currency, current_currency, context=None):
         line_total, rec_list_ids = super(account_voucher, self).voucher_move_line_create(cr, uid, voucher_id, line_total, move_id, company_currency, current_currency, context=context)
         
-        import wdb
-        wdb.set_trace()
-        
         move_brw = self.pool.get('account.move').browse(cr, uid, move_id, context=context)
         voucher = self.pool.get('account.voucher').browse(cr, uid, voucher_id, context=context)
-        i = 0
+        i = 1
         for v_line in voucher.line_ids:
             move_line = move_brw.line_id[i]
             if move_line.account_id.type != 'payable':
