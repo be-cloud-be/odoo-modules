db332571990c61ffeb8ba7f08d6f2e5327a749d8
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2015-09-27 09:13:00 +0000

    migrate to new API mode

--- account_bank_statement_import_multiline/account_bank_statement_import_multiline.py
+++ account_bank_statement_import_multiline/account_bank_statement_import_multiline.py
@@ -31,9 +31,7 @@ class AccountBankStatementImport(models.TransientModel):
     def import_file(self):
         """ Process the file chosen in the wizard, create bank statement(s) and go to reconciliation. """
 
-        import wdb
-        wdb.set_trace()
-
+        # Fix data charset and other small things
         data_file = self.with_context(active_id=self.ids[0]).data_file
         data = base64.b64decode(data_file)
         encoding = chardet.detect(data)
@@ -41,39 +39,9 @@ class AccountBankStatementImport(models.TransientModel):
         # REMOVE BOM if present because it depends of the platform use to access multiline.
         if data[:3] == codecs.BOM_UTF8:
             data = data[3:]
+        self.data_file = base64.encode(date)
 
-        # Parse the file and build a list of statement organised as a tree [currency_code][account_number][statement_id]
-        all_statements = self.with_context(active_id=self.ids[0])._parse_file(data)
-        all_statement_ids = []
-        all_notifications = []
-        for statement_key in all_statements.keys():
-            currency_code, account_number, statement_id = statement_key
-            stmts_vals = all_statements[statement_key]
-            # Check raw data
-            self._check_parsed_data([stmts_vals])
-            # Try to find the journal and currency in odoo
-            currency_id, journal = self._find_additional_data(currency_code, account_number)
-            # If no journal found, ask the user about creating one
-            if not journal:
-                return self.with_context(active_id=self.ids[0])._journal_creation_wizard(currency, account_number)
-            # Prepare statement data to be used for bank statements creation
-            stmts_vals = self._complete_stmts_vals(stmts_vals, journal, account_number)
-            # Create the bank statements
-            statement_ids, notifications = self._create_bank_statements(stmts_vals)
-            all_statement_ids.append(statement_ids)
-            all_notifications.append(notifications)
-
-        # Finally dispatch to reconciliation interface
-        action = self.env.ref('account.action_bank_reconcile_bank_statements')
-        return {
-            'name': action.name,
-            'tag': action.tag,
-            'context': {
-                'statement_ids': all_statement_ids,
-                'notifications': all_notifications
-            },
-            'type': 'ir.actions.client',
-        }
+        return super(AccountBankStatementImport, self).import_file()
 
     def _check_csv(self,file):
         try:
