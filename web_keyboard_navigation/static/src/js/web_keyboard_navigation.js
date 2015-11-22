b26258f6899983e433a7c7f85f702d14f62cc592
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2015-11-22 20:46:42 +0000

    remove debug log

--- web_keyboard_navigation/static/src/js/web_keyboard_navigation.js
+++ web_keyboard_navigation/static/src/js/web_keyboard_navigation.js
@@ -1,7 +1,6 @@
 $.ctrl = function(key, callback, args) {
     $(document).keydown(function(e) {
         if(!args) args=[]; // IE barks when args is null 
-        console.log(e.keyCode)
         if((e.keyCode == key.charCodeAt(0) || e.keyCode == key) && e.ctrlKey) {
             callback.apply(this, args);
             return false;
