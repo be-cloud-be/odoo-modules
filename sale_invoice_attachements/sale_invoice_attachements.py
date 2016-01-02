3ac45e7caf20221979406bd2409e459c49acc849
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-01-02 20:06:16 +0000

    new module to count attachements

---
+++ sale_invoice_attachements/sale_invoice_attachements.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 be-cloud.be
+#                       Jerome Sonnet <jerome.sonnet@be-cloud.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import api, fields, models, _
+from openerp.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.one
+    def count_attachments(self):
+        obj_attachment = self.pool.get('ir.attachment')
+        for record in self:
+            record.attachment_count =0
+            attachment_ids = obj_attachment.search([('res_model','=','<<your model name>>'),('res_id','=',record.id)]).ids
+            if attachment_ids:
+                record.attachment_count =len(attachment_ids)
+                
+    attachment_count = fields.Integer(string="Attachment Count" ,compute="count_attachments")
