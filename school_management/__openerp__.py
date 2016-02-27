d1160a0fb2a3575c0a953789dd1c095a8d4d9cd9
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-02-27 17:22:28 +0000

    add courses

--- school_management/__openerp__.py
+++ school_management/__openerp__.py
@@ -25,7 +25,7 @@
     'author': 'be-Cloud.be (Jerome Sonnet)',
     'website': '',
     'category': 'Association',
-    'depends': [],
+    'depends': ['base'],
     'init_xml': [],
     'update_xml': [
         'views/res_partner_view.xml',
