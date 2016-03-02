8182e83f5a5fe0ad4e4ea4ddb0fc0440cb1b97a0
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-03-02 15:10:44 +0000

    add year

--- school_management/models/assignment.py
+++ school_management/models/assignment.py
@@ -29,7 +29,7 @@ class assignment(models.Model):
     '''Assignment'''
     _name = 'school.assignment'
     
-    year_id = fields.Many2one(string='Year',related='program_id.year_ids', store=True)
+    year_id = fields.Many2one(string='Year',related='program_id.year_id', store=True)
     program_id = fields.Many2one('school.program', string='Program')
     course_id = fields.Many2one('school.course', string='Course')
     teacher_id = fields.Many2one('res.partner', string='Teacher', domain="[('teacher', '=', '1')]")
