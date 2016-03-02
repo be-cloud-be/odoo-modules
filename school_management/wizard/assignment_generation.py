f951e98751d2d63cfb3cd0e00ec547c62d3fa4e2
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-03-02 16:27:31 +0000

    add year

--- school_management/wizard/assignment_generation.py
+++ school_management/wizard/assignment_generation.py
@@ -39,8 +39,8 @@ class AccountCommonReport(models.TransientModel):
         
         res = self.env.cr.fetchall()
         for (program_id,course_id) in res:
-            try:
-                _logger.info('Create assigment %s - %s',program_id,course_id)
-                self.env['school.assigment'].create({'program_id':program_id, "course_id":course_id})
+            #try:
+            _logger.info('Create assigment %s - %s',program_id,course_id)
+            self.env['school.assigment'].create({'program_id':program_id, "course_id":course_id})
             #except Exception as e:
-            #    _logger.info(e)
+            #    _logger.error('Error during creation of assingment %s',e)
