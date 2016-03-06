f89c651dee9a08f9b2e83fa5c3222a8c73315f80
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-03-06 07:59:50 +0000

    add career history

---
+++ life/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (c) 2010-2012 Elico Corp. All Rights Reserved.
+#
+#    Author: Yannick Gouin <yannick.gouin@elico-corp.com>
+#            Jerome Sonnet <jerome.sonnet@be-cloud.be> port to 9.0
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Life',
+    'category' : 'Accounting & Finance',
+    'version': '0.1',
+    'author': 'be-cloud.be (Jerome Sonnet)',
+    'description' : """
+Module to manage life insurance contracts.
+==========================================
+
+    """,
+    'data': ['views/res_partner_view.xml',
+             #'security/ir.model.access.csv',
+             ],
+    'depends': [],
+    'demo': [],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+}
---
+++ life/models/res_partner.py
@@ -0,0 +1,62 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 be-cloud.be
+#                       Jerome Sonnet <jerome.sonnet@be-cloud.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+
+from openerp import api, fields, models, _
+from openerp.exceptions import UserError
+from openerp.tools.safe_eval import safe_eval
+
+_logger = logging.getLogger(__name__)
+
+class Partner(models.Model):
+    '''Partner'''
+    _inherit = 'res.partner'
+    
+    grade_id = fields.Many2one("life.grade",string="Grade")
+    barem_id = fields.Many2one("life.barem",string="Bareme")
+    position = fields.Char(string="Position")
+    
+    career_history_ids = fields.One2many('life.career_history', string="Career History")
+    
+class CareerHistory(models.Model):
+    '''Career History'''
+    _name = 'life.career_history'
+    _description = 'Career History'
+    
+    date_from = fields.Date(string="Date From")
+    date_to = fields.Date(string="Date To")
+    grade_id = fields.Many2one("life.grade",string="Grade")
+    barem_id = fields.Many2one("life.barem",string="Bareme")
+    position = fields.Char(string="Position")
+    
+class Grade(models.Model):
+    '''Grade'''
+    _name = 'life.grade'
+    _description = 'Grade'
+    
+    name = fields.Char(string="Name")
+    
+class Bareme(models.Model):
+    '''Bareme'''
+    _name = 'life.bareme'
+    _description = 'Bareme'
+    
+    name = fields.Char(string="Name")
