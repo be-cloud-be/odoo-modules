23aeb17c5843d5350fd76b22cf988af50800c5bb
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-03-06 09:58:30 +0000

    add career history

--- life/models/res_partner.py
+++ life/models/res_partner.py
@@ -34,7 +34,7 @@ class Partner(models.Model):
     pay_grid_id = fields.Many2one("life.pay_grid",string="Pay Grid")
     
     sex = fields.Selection([('m', 'Male'),('f', 'Female')])
-    family_status = fields.Selection([('s', 'Solo'),('m', 'Maried'),('c', 'Legal cohabitor')])
+    family_status = fields.Selection([('s', 'Solo'),('m', 'Married'),('c', 'Legal cohabitor')])
     partner_birthdate = fields.Date(string="Partner Birthdate")
     children = fields.Integer(string="Number of children")
  
