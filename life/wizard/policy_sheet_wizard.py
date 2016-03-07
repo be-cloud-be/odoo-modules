101956b683488ea869cab68ca0c93d3dcf591eec
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-03-07 22:05:32 +0000

    add policy sheet report

--- life/wizard/policy_sheet_wizard.py
+++ life/wizard/policy_sheet_wizard.py
@@ -50,11 +50,12 @@ class ReportPolicySheet(models.AbstractModel):
     
     @api.multi
     def render_html(self, data):
+        _logger.info('ender_html')
         _logger.info(data)
         docargs = {
-            'doc_ids': data.policy_id,
+            'doc_ids': data['policy_id'],
             'doc_model': 'life.policy',
-            'docs': self.env['life.policy'].browse(data.policy_id),
+            'docs': self.env['life.policy'].browse(data['policy_id']),
         }
         _logger.info(docargs)
         return self.env['report'].render('life.report_policy_sheet', docargs)
