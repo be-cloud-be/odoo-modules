d8756d490510ccf5ee43116684dd3cc397ce9a1f
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-03-16 19:31:13 +0000

    fix data import order

--- life/models/res_partner.py
+++ life/models/res_partner.py
@@ -74,7 +74,7 @@ class Partner(models.Model):
         # TODO : compute the career duration in complete year, shoud be a fragment ?
         if self.retirement_date :
             dt_retirement_date = datetime.strptime(self.retirement_date, DEFAULT_SERVER_DATE_FORMAT)
-            self.remaining_career_duration = (dt_retirement_date - date.today().date()).days/365.25
+            self.remaining_career_duration = (dt_retirement_date - date.today()).days/365.25
         else :
             self.remaining_career_duration = None
 
