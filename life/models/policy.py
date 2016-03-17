409affa10a4bc9df885d9f2c253431d00c22db96
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-03-17 10:53:10 +0000

    policy report

--- life/models/policy.py
+++ life/models/policy.py
@@ -37,8 +37,8 @@ class Policy(models.Model):
     _description = 'Policy'
 
     number = fields.Integer(string="Policy Number")
-    policy_holder_id = fields.Many2one('res.partner',string='Policy Holder',related='insured_person_id.company_id',store='True')
     insured_person_id = fields.Many2one('res.partner',string='Insured Person')
+    policy_holder_id = fields.Many2one('res.partner',string='Policy Holder',related='insured_person_id.company_id',store='True')
     
     name = fields.Char(string="Name",compute='_compute_name')
 
