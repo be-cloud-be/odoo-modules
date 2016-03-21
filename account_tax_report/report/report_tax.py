a23734e24c9c648c56eb2d00818daa1e73fb7ab8
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-03-21 09:16:02 +0000

    account tax first commit

---
+++ account_tax_report/report/report_tax.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 be-cloud.be
+#                       Jerome Sonnet <jerome.sonnet@be-cloud.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+
+from openerp import api, fields, models, _
+from openerp.exceptions import UserError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class ReportFinancial(models.AbstractModel):
+    _name = 'report.account.report_tax'
+    
+    def get_tax_lines(self, data):
+        lines = []
+        
+        
+        return lines
+    
+    @api.multi
+    def render_html(self, data):
+        self.model = self.env.context.get('active_model')
+        docs = self.env[self.model].browse(self.env.context.get('active_id'))
+        report_lines = self.get_tax_lines(data.get('form'))
+        docargs = {
+            'doc_ids': self.ids,
+            'doc_model': self.model,
+            'data': data['form'],
+            'docs': docs,
+            'time': time,
+            'get_tax_lines': report_lines,
+        }
+        return self.env['report'].render('account.report_tax', docargs)
