4acdcd8308c3f575d893e3ce5efbcb96a590ae57
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-06-01 08:48:44 +0000

    add sticky header

---
+++ web_list_view_sticky/static/src/js/web_list_view_sticky.js
@@ -0,0 +1,19 @@
+openerp.web_list_view_sticky = function (instance) {
+    var _t = instance.web._t,
+        _lt = instance.web._lt;
+    var QWeb = instance.web.qweb;
+
+    //Sticky Table Header
+    instance.web.ListView.include({
+        load_list: function () {
+            var self = this;
+            self._super.apply(this, arguments);
+	    var scrollArea = self.ViewManager.$el;
+	    if(scrollArea){
+               self.$el.find('table.oe_list_content').each(function(){
+                    $(this).stickyTableHeaders({scrollableArea: scrollArea, leftOffset: scrollArea})
+               });
+	    }
+        },
+    });
+};
