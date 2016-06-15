a19ddcc1bd47dec348d98ddd3465e39d02fe030a
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-06-15 09:34:13 +0000

    First doc

---
+++ school_documentation/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 be-cloud.be
+#                       Jerome Sonnet <jerome.sonnet@be-cloud.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'School documentation',
+    'version': '0.1',
+    'license': 'AGPL-3',
+    'author': 'be-Cloud.be (Jerome Sonnet)',
+    'website': '',
+    'category': 'School Management',
+    'depends': ['school_management'],
+    'init_xml': [],
+    'update_xml': [
+        'views/documentation_view.xml',
+    ],
+    'demo_xml': [],
+    'description': '''
+        This modules add documenation for a school management.
+    ''',
+    'qweb': [],
+    'active': False,
+    'installable': True,
+    'application': True,
+}
---
+++ school_documentation/source/index.rst
@@ -0,0 +1,22 @@
+.. Gestion des études documentation master file, created by
+   sphinx-quickstart on Mon Jun 13 09:02:29 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Bienvenu à la documentation de la Gestion des études
+====================================================
+
+Contenu:
+
+.. toctree::
+   :maxdepth: 2
+   
+   programmes.rst
+   
+Indexes et Tables
+=================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
