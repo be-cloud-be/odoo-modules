92fba774d9b4583fcce8189b7c89c954b3a9ce3b
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-07-25 14:25:32 +0000

    kind of working first version

--- school_registration/wizard/register_next.py
+++ school_registration/wizard/register_next.py
@@ -30,33 +30,76 @@ class RegisterNext(models.TransientModel):
     _name = "school.register_next_wizard"
     _description = "Register Next Wizard"
     
+    state = fields.Selection([('awarded', 'awarded'),('failed', 'failed'),('rework', 'rework'),('anticipate', 'anticipate'),('confirm','confirm')]) 
+    
     init_bloc_id = fields.Many2one('school.individual_bloc', string="Initial Bloc", required=True, readonly=True)
     
-    student_name = fields.Char('Student', related="init_bloc_id.student_name", readonly=True)
-    
+    yeard_id = fields.Many2one('school.year', related="init_bloc_id.year_id", readonly=True)
+    student_id = fields.Many2one('res.partner', related="init_bloc_id.student_id", readonly=True)
     init_source_bloc_name = fields.Char(string="Initial Source Bloc", readonly=True, related="init_bloc_id.source_bloc_name")
     
-    new_bloc_id = fields.Many2one('school.individual_bloc', string="New Bloc")
-    
+    new_bloc_id = fields.Many2one('school.individual_bloc', string="New Bloc", readonly=True)
     new_bloc_name = fields.Char(string="New Source Bloc", readonly=True, related="new_bloc_id.source_bloc_name")
     
     course_group_ids = fields.One2many('school.individual_course_group', related='new_bloc_id.course_group_ids', string='Courses Groups')
     
+    rework_course_group_ids = fields.Many2many('school.course_group', 'rework_course_group_wizard_rel', 'course_group_id', 'wizard_id', string='Rework Course')
+    
     anticipated_course_group_ids = fields.Many2many('school.course_group', 'anticipated_course_group_wizard_rel', 'course_group_id', 'wizard_id', string='Anticipated Course')
     
+    total_credits = fields.Integer(compute='_get_courses_total', string='Credits')
+    total_hours = fields.Integer(compute='_get_courses_total', string='Hours')
+    
+    @api.one
+    @api.depends('course_group_ids')
+    def _get_courses_total(self):
+        total_hours = 0.0
+        total_credits = 0.0
+        for course_group in self.course_group_ids:
+            total_hours += course_group.total_hours
+            total_credits += course_group.total_credits
+        for course_group in self.rework_course_group_ids:
+            total_hours += course_group.total_hours
+            total_credits += course_group.total_credits
+        for course_group in self.anticipated_course_group_ids:
+            total_hours += course_group.total_hours
+            total_credits += course_group.total_credits
+        self.total_hours = total_hours
+        self.total_credits = total_credits
+
     @api.model
     def default_get(self, fields):
-        import wdb
-        wdb.set_trace()
         res = super(RegisterNext, self).default_get(fields)
         init_bloc_id = self.env['school.individual_bloc'].browse(res.get('init_bloc_id'))
-        ## We go the real logic to create the new program
+        if init_bloc_id.state == 'failed':
+            res['state'] = 'failed'
+            new_bloc_source_id = self.env['school.bloc'].search([('program_id', '=', self.init_bloc_id.source_bloc_id.program_id.id),('level','=',int(self.init_bloc_id.source_bloc_id.level)+1)])
+        else:
+            res['state'] = 'awarded'
+            new_bloc_source_id = self.env['school.bloc'].search([('program_id', '=', self.init_bloc_id.source_bloc_id.program_id.id),('level','=',int(self.init_bloc_id.source_bloc_id.level)+1)])
         new_bloc_source_id = self.env['school.bloc'].search([('program_id', '=', init_bloc_id.source_bloc_id.program_id.id),('level','=',int(init_bloc_id.source_bloc_id.level)+1)])
         if new_bloc_source_id:
-            program = self.env['school.individual_bloc'].create({'year_id':init_bloc_id.year_id.next.id,'student_id': init_bloc_id.student_id.id,'bloc_source_id':new_bloc_source_id[0].id,'program_id':init_bloc_id.program_id.id})
+            program = self.env['school.individual_bloc'].create({'year_id':init_bloc_id.year_id.next.id,'student_id': init_bloc_id.student_id.id,'source_bloc_id':new_bloc_source_id[0].id,'program_id':init_bloc_id.program_id.id})
+            program.assign_source_bloc()
             res['new_bloc_id'] = program.id
         return res
 
+    @api.onchange('state')
+    @api.one
+    def onchange_state(self):
+        # Rework look into previous bloc for not acquiered CG
+        if self.state == 'rework':
+            for group in self.init_bloc_id.course_group_ids:
+                if not group.acquiered:
+                    self.rework_course_group_ids.push(group.clone())
+            return
+        # Confirm merge all into the new bloc for review
+        if self.state == 'confirm':
+            self.new_bloc_id.course_group_ids.append(self.rework_course_group_ids)
+            self.new_bloc_id.course_group_ids.append(self.anticipated_course_group_ids)
+            
+    
+            
 class RegisterNextLine(models.TransientModel):   
     _name = 'school.individual_course_group_proxy'
     
