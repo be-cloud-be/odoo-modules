aa901ecccb0d4df7174ca187490dbf5590b31603
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-10-28 11:07:08 +0000

    student group

--- school_student_group/models/student_group.py
+++ school_student_group/models/student_group.py
@@ -30,7 +30,7 @@ class StudentGroup(models.Model):
     _description = 'Student Group'
     _inherit = ['mail.thread','school.year_sequence.mixin']
     
-    _order = 'responsible_id, course_id'
+    _order = 'responsible_id, year_id, name'
     
     year_id = fields.Many2one('school.year', string='Year', required=True, default=lambda self: self.env.user.current_year_id)
     responsible_id = fields.Many2one('res.partner', string='Responsible', domain="[('type','=','contact')]", required=True)
@@ -53,7 +53,10 @@ class StudentGroup(models.Model):
                 studentg.short_name = studentg.title
     
     staff_ids = fields.Many2many('res.partner', 'group_staff_rel', 'group_id', 'staff_id', string='Staff', domain="[('type','=','contact')]")
+    
     student_ids = fields.Many2many('res.partner', 'group_student_rel', 'group_id', 'student_id', string='Student', domain="[('student','=','1')]")
+    
+    individual_course_ids = fields.Many2many('school.individual_course', 'group_individual_course_rel', 'group_id', 'course_id', string='Individual Course')
     
     student_count = fields.Integer(compute='_compute_student_count', string="Student Count")
 	
@@ -88,21 +91,152 @@ class StudentGroup(models.Model):
                         # 'student_ids' : (0, _, student_ids.ids), TODO DOES NOT WORK, WHY ??
                     })
                     new_group.student_ids = student_ids
-                    
+
+    type = fields.Selection([('L','LinkedCourses'),('F','FreeCourses'),('P','Project'),('O','Others')],string="Group Type",default="F", required=True)
+    
+    course_ids = fields.Many2many('school.course', 'group_course_rel', 'group_id', 'course_id', string='Courses', domain=lambda(self): "[('teacher_ids', '=', responsible_id)]" if self.type in ['L','F'] else "")
+    
+    domain_id = fields.Many2one('school.domain', computed='_compute_course_info', store=True)
+    speciality_id = fields.Many2one('school.speciality', computed='_compute_course_info', store=True)
+    cycle_id = fields.Many2one('school.cycle', computed='_compute_course_info', store=True)
+    
+    @api.one
+    @api.depends('course_ids')
+    def _compute_course_info(self):
+        if len(self.course_ids) > 0:
+            self.domain_id = self.course_ids[0].domain_id
+            self.speciality_id = self.course_ids[0].speciality_id
+            self.cycle_id = self.course_ids[0].cycle_id
+        else:
+            self.domain_id = None
+            self.speciality_id = None
+            self.cycle_id = None
+        
+    name = fields.Char(string='Name', compute='_compute_name', store=True)
+    short_name = fields.Char(string='Short Name', compute='_compute_name')
+    title = fields.Char(string='Title')
+    
+    @api.one
+    @api.depends('year_id', 'title', 'course_ids', 'responsible_id')
+    def _compute_name(self):
+        if self.type == 'L':
+            title = self.course_ids[0].name if len(self.course_ids) > 0 else ""
+            self.name = '%s - %s - %s' % (self.year_id.short_name, self.responsible_id.name, title)
+            self.short_name = title
+        else:
+            self.name = '%s - %s' % (self.year_id.short_name, self.title)
+            self.short_name = self.title
+
+    participant_ids = fields.Many2many('res.partner', 'school_group_participants_rel', 'group_id', 'res_partner_id', string='Participipants')
+    picked_participant_ids = fields.Many2many('res.partner', 'school_group_res_partner_rel', 'group_id', 'res_partner_id', string='Picked Participipants')
+    participant_count = fields.Integer(string="Participant Count", compute="_compute_participant_count")
+
+    individual_course_ids = fields.Many2many('school.individual_course', 'school_group_individual_course_rel', 'group_id', 'individual_course_id', string='Individual Courses', domain="[('year_id','=',year_id)]")
+    
+    event_ids = fields.One2many('calendar.event','student_group_id',string='Events')
+    
+    @api.onchange('type')
+    def onchange_type(self):
+        # If type change we reset everything : TODO add a user confirmation.
+        self.course_ids = []
+        self.participant_ids = []
+        self.picked_participant_ids = []
+        self.individual_course_ids = []
+        return {
+            'domain' : {'individual_course_ids': [('year_id','=',self.year_id.id)]}
+        }
+    
+    @api.onchange('responsible_id')
+    def onchange_responsible_id(self):
+        if self.type in ['L','F'] :
+            return {
+                'domain': {'course_ids': [('teacher_ids', '=', self.responsible_id.id)]}
+            }
+        
+    @api.onchange('course_ids')
+    def onchange_course_ids(self):
+        if self.course_ids:
+            if self.type == 'L':
+                self.individual_course_ids = self.env['school.individual_course'].search([('year_id','=',self.year_id.id),('source_course_id','in',self.course_ids.ids)])
+            elif self.type == 'F':
+                for ic in self.individual_course_ids:
+                    if ic.source_course_id not in self.course_ids:
+                        raise ValidationError(_('Individual Course %s does not match the course selection, please remove it before changing the Courses' % (ic.name)))
+            else:
+                raise ValidationError(_('Cannot select Course on this type of group.'))
+            return {
+                'domain': {'individual_course_ids': [('year_id','=',self.year_id.id),('source_course_id', '=', self.course_ids.ids)]}
+            }
+
+    @api.onchange('picked_participant_ids')
+    def onchange_picked_participant_ids(self):
+        if self.type == 'L':
+            if len(self.picked_participant_ids) > 0 :
+                raise ValidationError(_('Cannot pick participants on a LinkedCourses type of group, please set Courses instead.'))
+        elif self.type == 'F':
+            if len(self.picked_participant_ids) > 0 :
+                raise ValidationError(_('Cannot pick participants on a FreeCourses type of group, please add Individual Courses instead.'))
+        else:
+            self.participant_ids = self.picked_participant_ids
+        self.participant_count = len(self.participant_ids)
+            
+    @api.onchange('individual_course_ids')
+    def onchange_individual_course_ids(self):
+        if self.type == 'L':
+            self.participant_ids = self.individual_course_ids.mapped('student_id')
+        elif self.type == 'F':
+            for ic in self.individual_course_ids:
+                if ic.source_course_id not in self.course_ids:
+                    raise ValidationError(_('Individual Course %s does not match the course selection' % (ic.name)))
+            self.participant_ids = self.individual_course_ids.mapped('student_id')
+        elif self.type == 'P':
+            student_ids = self.individual_course_ids.mapped('student_id')
+            for student in student_ids:
+                if student not in self.participant_ids:
+                    self.participant_ids |= student
+        else:
+            raise ValidationError(_('Cannot select Individual Courses on this type of group.'))
+        self.participant_count = len(self.participant_ids)
+
+    @api.one    
+    def _compute_participant_count(self):
+        self.participant_count = len(self.participant_ids)
+
+    @api.multi
+    def action_participants_list(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Participants',
+            'res_model': 'res.partner',
+            'domain': [('id', 'in', self.participant_ids.ids)],
+            'view_mode': 'tree',
+        }
+
+    @api.one
+    def update_linked_students(self):
+        if self.type == 'L':
+            self.individual_course_ids = self.env['school.individual_course'].search([('year_id','=',self.year_id.id),('source_course_id','in',self.course_ids.ids)])
+            self.participant_ids = self.individual_course_ids.mapped('student_id')
+            self.participant_count = len(self.participant_ids)
+            
     @api.model
     def generate_all_student_course_group(self):
         year_id = self.env.user.current_year_id
         all_blocs = self.env['school.bloc'].search([('year_id','=',year_id.id)])
-        # We are going through all courses
+        # We are going through all courses, of all current blocs
         all_course_ids = set()
         for bloc in all_blocs:
             for course_group in bloc.course_group_ids:
                 for course in course_group.course_ids:
                     all_course_ids.add(course.id)
+        # For each course we look for students and create the corresponding groups
         for course_id in all_course_ids :      
             course = self.env['school.course'].browse(course_id)
-            student_ids = self.env['school.individual_course'].search([('year_id','=',year_id.id),('source_course_id','=',course.id)]).mapped('student_id')
+            individual_course_ids = self.env['school.individual_course'].search([('year_id','=',year_id.id),('source_course_id','=',course.id)])
+            student_ids = individual_course_ids.mapped('student_id')
             if len(student_ids) > 0 :
+                # Only one teacher for this course
                 if len(course.teacher_ids) == 1:
                     old_group = self.env['school.student_group'].search([('responsible_id','=',course.teacher_ids.ids[0]),('year_id','=',year_id.id),('course_id','=',course.id)])
                     if not old_group:
@@ -115,39 +249,45 @@ class StudentGroup(models.Model):
                         })
                         # set all students in as only one theacher
                         new_group.student_ids = student_ids
+                        new_group.individual_course_ids = individual_course_ids
+                # Several teachers for this course
                 elif len(course.teacher_ids) > 1:
                     for teacher in course.teacher_ids:
                         old_group = self.env['school.student_group'].search([('responsible_id','=',teacher.id),('year_id','=',year_id.id),('course_id','=',course.id)])
                         if not old_group:
+                            # We create a group for each teacher
                             new_group = self.env['school.student_group'].create({
                                 'type': 'C',
                                 'year_id': year_id.id,
                                 'course_id': course.id,
                                 'responsible_id': teacher.id,
                             })
-                            # try to guess the teacher based on previous year
+                            # Try to find the group of the previous year
                             for student_id in student_ids:
-                                old_course = self.env['school.individual_course'].search([('student_id','=',student_id.id),('year_id','=',year_id.previous.id),('title', '=', course.title),('teacher_id','=',teacher.id)])
-                                if old_course:
-                                    new_group.student_ids |= student_id
-
+                                previous_group = self.env['school.student_group'].search([('responsible_id','=',teacher.id),('year_id','=',year_id.previous.id),('course_id.title','=',course.title),('student_ids','=',student_id.id)])
+                                if previous_group:
+                                    # Set students that were in the previous group
+                                    new_group.student_ids = previous_group.student_ids & student_ids
+                                    break
+                                
 class Course(models.Model):
     '''Course'''
     _inherit = 'school.course'
     
     group_ids = fields.One2many('school.student_group', 'course_id', string='Groups')
     
-    
 class IndividualCourse(models.Model):
     '''Individual Course'''
     _inherit = 'school.individual_course'
     
-    @api.depends('teacher_choice_id','source_course_id.teacher_ids')
+    @api.depends('year_id','source_course_id')
     @api.one
     def compute_teacher_id(self):
-        student_group = self.env['school.student_group'].search([('year_id','=',self.year_id.id),('course_id','=',self.source_course_id.id),('student_ids','=',self.id)])
+        student_group = self.env['school.student_group'].search([('year_id','=',self.year_id.id),('course_ids','=',self.source_course_id.id),('participant_ids','=',self.student_id.id)])
+        if len(student_group) > 1:
+            import wdb
+            wdb.set_trace()
         if student_group:
             self.teacher_id = student_group.responsible_id
         else:
             self.teacher_id = None
-        
