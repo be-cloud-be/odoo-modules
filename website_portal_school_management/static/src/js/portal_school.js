9212443016082450b0a1b6d77f26841d3899f7ef
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-10-30 07:47:14 +0000

    improvements

--- website_portal_school_management/static/src/js/portal_school.js
+++ website_portal_school_management/static/src/js/portal_school.js
@@ -21,4 +21,36 @@ odoo.define('school_evaluations.action_school_portal_main', function (require) {
             })
         }); 
     });
+    
+    function setModalMaxHeight(element) {
+      this.$element     = $(element);  
+      this.$content     = this.$element.find('.modal-content');
+      var borderWidth   = this.$content.outerHeight() - this.$content.innerHeight();
+      var dialogMargin  = $(window).width() < 768 ? 20 : 60;
+      var contentHeight = $(window).height() - (dialogMargin + borderWidth);
+      var headerHeight  = this.$element.find('.modal-header').outerHeight() || 0;
+      var footerHeight  = this.$element.find('.modal-footer').outerHeight() || 0;
+      var maxHeight     = contentHeight - (headerHeight + footerHeight);
+    
+      this.$content.css({
+          'overflow': 'hidden'
+      });
+      
+      this.$element
+        .find('.modal-body').css({
+          'max-height': maxHeight,
+          'overflow-y': 'auto'
+      });
+    }
+    
+    $('.modal').on('show.bs.modal', function() {
+      $(this).show();
+      setModalMaxHeight(this);
+    });
+    
+    $(window).resize(function() {
+      if ($('.modal.in').length != 0) {
+        setModalMaxHeight($('.modal.in'));
+      }
+    });
 });
