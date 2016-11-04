1ad818827b2d3839654e8c015f7af6716115cdfa
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-11-04 07:41:29 +0000

    add pdf template

---
+++ account_invoice_pdf/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (c) 2010-2012 Elico Corp. All Rights Reserved.
+#
+#    Author: Yannick Gouin <yannick.gouin@elico-corp.com>
+#            Jerome Sonnet <jerome.sonnet@be-cloud.be> port to 9.0
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Account Invoice as PDF',
+    'version': '1.1',
+    'category': 'Tools',
+    'description': """
+    Add another template for invoice in PDF.
+    """,
+    "author": "be-cloud.be (Jerome Sonnet)",
+    "website": "http://www.be-cloud.be",
+    'depends': ['account'],
+    'init_xml': [],
+    'data': [
+            
+    ],
+    'installable': True,
+    'active': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
