9204ca25f0d53943cd219f8082c8c8f854ec6bef
jerome.sonnet@be-cloud.be <jerome.sonnet@be-cloud.be>
2016-11-08 07:54:29 +0000

    add report pdf

--- account_timsheet_report/model/account_timesheet_report.py
+++ account_timsheet_report/model/account_timesheet_report.py
@@ -59,7 +59,7 @@ class AccountTimesheetReport(models.AbstractModel):
             lines.append({
                 'id' : analytic_line.id,
                 'name' : analytic_line.name,
-                'type' : 'partner_id',
+                'type' : 'line',
                 'footnotes': {},
                 'unfoldable' : False,
                 'columns' : [analytic_line.date, analytic_line.unit_amount],
