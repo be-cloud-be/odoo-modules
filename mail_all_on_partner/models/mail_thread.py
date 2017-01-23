0dce779dc269457a04960a6911b0453dd63c88d9
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-01-23 13:31:25 +0000

    First Try

---
+++ mail_all_on_partner/models/mail_thread.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 be-cloud.be
+#                       Jerome Sonnet <jerome.sonnet@be-cloud.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+
+from openerp import api, fields, models, _
+from openerp.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+class MailThread(models.AbstractModel):
+    _inherit = 'mail.thread'
+    
+    all_message_ids = fields.One2many('mail.message', compute='_get_all_message_ids')
+    
+    @api.one
+    def _get_all_message_ids(self):
+        if self._name != 'res.partner':
+            return self.message_ids
+        else:
+            linked_messages = self.env['mail.message'].search(['|',('author_id','=',self.id),('partner_ids','in',self.id)])
+            return self.message_ids + linked_messages
