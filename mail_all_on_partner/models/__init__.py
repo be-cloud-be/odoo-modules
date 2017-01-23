b9bdc53ca9f1f387ea041af3f4d6bfe3407847ff
Jerome Sonnet (be-cloud.be) <jerome.sonnet@be-cloud.be>
2017-01-23 13:40:25 +0000

    First Try

--- mail_all_on_partner/models/__init__.py
+++ mail_all_on_partner/models/__init__.py
@@ -18,4 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import mail_thread
+import res_partner
