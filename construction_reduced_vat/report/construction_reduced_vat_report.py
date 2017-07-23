eb6589122e97f119dc6d01c28ebe9712f1bd37f1
Jerome Sonnet (be-cloud.be) <jerome.sonnet@imply.lu>
2017-07-23 06:09:40 +0000

    fix quarter

--- construction_reduced_vat/report/construction_reduced_vat_report.py
+++ construction_reduced_vat/report/construction_reduced_vat_report.py
@@ -54,6 +54,8 @@ class ReducedVATAgreementReport(models.Model):
         tools.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute("""CREATE or REPLACE VIEW %s as (
             SELECT inv.id, agg.agreement_code, cust.matricule, s_addr.zip, inv.date_invoice as date, inv.move_name as number, inv.amount_untaxed, inv.amount_tax, inv.company_id
+            , to_char(date_trunc('quarter', current_date)::date - 1, 'yyyy-q') = to_char(inv.date_invoice, 'yyyy-q') as last_quarter
+            , to_char(current_date, 'yyyy-q') = to_char(inv.date_invoice, 'yyyy-q') as current_quarter            
             FROM account_invoice inv, res_partner cust, construction_reduced_vat_agreement agg, construction_building_asset ba, construction_building_site bs, res_partner s_addr
             WHERE inv.partner_id = cust.id AND inv.reduced_vat_agreement_id = agg.id AND inv.building_asset_id = ba.id AND ba.site_id = bs.id AND bs.address_id = s_addr.id
         )""" % (self._table))
