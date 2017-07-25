e77d5b783f0fca90d6437a1179cb5aa1f73ef2f9
Jerome Sonnet (be-cloud.be) <jerome.sonnet@imply.lu>
2017-07-25 06:07:45 +0000

    restore files

---
+++ document_gdrive/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 be-cloud.be Jerome Sonnet <jerome.sonnet@be-cloud.be>
+# Copyright 2016 Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import res_config
